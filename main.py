fdd81278aa9722949b3cec52f3f3c5d641858b0b
vindex10 <vindex10@gmail.com>
2022-04-02 22:05:20 +0200

    transition to forward mapping variable. allow many chat channel pairs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import json
 import logging
 from telegram.ext import Updater
 from telegram.ext import MessageHandler
@@ -7,34 +8,45 @@ from telegram.ext.filters import Filters
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
-CHAT_ID_FROM = int(os.environ["CHAT_ID_FROM"])
-CHANNEL_ID_TO = int(os.environ["CHANNEL_ID_TO"])
+FORWARD_MAPPING = json.loads(os.environ["FORWARD_MAPPING"])
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 
 updater = Updater(token=BOT_TOKEN)
 dispatcher = updater.dispatcher
 
 
-def forward_forum22_message(update, context):
-    author = update.message.from_user.name
-    text = update.message.text
-    full_text = f"""
-{author} asks:
+def get_forward_callback(chat_from, channel_to):
+    def callback(update, context):
+        author = update.message.from_user.name
+        text = update.message.text
+        full_text = f"""
+{author} shares:
 
 {text}
 """
-    sent_message = context.bot.send_message(chat_id=CHANNEL_ID_TO, text=full_text)
-    new_full_text = f"""
+        sent_message = context.bot.send_message(chat_id=channel_to, text=full_text)
+        new_full_text = f"""
 {full_text}
 
 Thread: {sent_message.link}
 """
-    context.bot.send_message(chat_id=CHAT_ID_FROM, text=new_full_text)
-    update.message.delete()
+        context.bot.send_message(chat_id=chat_from, text=new_full_text)
+        update.message.delete()
+    return callback
 
 
-forum22_message_handler = MessageHandler(Filters.chat(CHAT_ID_FROM) & Filters.regex("#форум"),
-                                         forward_forum22_message)
-dispatcher.add_handler(forum22_message_handler)
+for config in FORWARD_MAPPING:
+    callback = get_forward_callback(config["chat_from"], config["channel_to"])
+    filters = Filters.chat(config["chat_from"])
+    filter_tags = None
+    tags =  config.get("tags")
+    if tags is not None:
+        filter_tags = Filters.regex(tags[0])
+        for tag in tags[1:]:
+            filter_tags = filter_tags | Filters.regex(tag)
+    if filter_tags is not None:
+        filters = filters & filter_tags
+    handler = MessageHandler(filters, callback)
+    dispatcher.add_handler(handler)
 
 updater.start_polling()
