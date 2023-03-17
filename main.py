6c89565729b0bd8a64bd725808e739b87f4a407f
Victor Ananyev <vindex10@gmail.com>
2023-03-17 22:38:29 +0100

    upgrade to newer tgbot version

--- main.py
+++ main.py
@@ -1,52 +1,59 @@
 import os
 import json
 import logging
-from telegram.ext import Updater
-from telegram.ext import MessageHandler
-from telegram.ext.filters import Filters
+from telegram.ext import ApplicationBuilder, MessageHandler, filters
+
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
-FORWARD_MAPPING = json.loads(os.environ["FORWARD_MAPPING"])
-BOT_TOKEN = os.environ["BOT_TOKEN"]
 
-updater = Updater(token=BOT_TOKEN)
-dispatcher = updater.dispatcher
+class Forward22Bot:
+    def __init__(self):
+        self.forward_mapping = json.loads(os.environ["FORWARD_MAPPING"])
+        bot_token = os.environ["BOT_TOKEN"]
+        self.app = ApplicationBuilder().token(bot_token).build()
+        self.init_handlers()
+
+    def run(self):
+        self.app.run_polling()
+
+    def init_handlers(self):
+        for config in self.forward_mapping:
+            callback = self.get_forward_callback(config["chat_from"], config["channel_to"])
+            one_filters = filters.Chat(chat_id=config["chat_from"])
+            filter_tags = None
+            tags = config.get("tags")
+            if tags is not None:
+                filter_tags = filters.Regex(tags[0])
+                for tag in tags[1:]:
+                    filter_tags = filter_tags | filters.Regex(tag)
+            if filter_tags is not None:
+                one_filters = one_filters & filter_tags
+            handler = MessageHandler(one_filters, callback)
+            self.app.add_handler(handler)
+
+    @staticmethod
+    def get_forward_callback(chat_from, channel_to):
+        async def callback(update, context):
+            author = update.message.from_user.name
+            text = update.message.text
+            full_text = f"""
+    {author} shares:
+
+    {text}
+    """
+            sent_message = await context.bot.send_message(chat_id=channel_to, text=full_text)
+            new_full_text = f"""
+    {full_text}
+
+    Thread: {sent_message.link}
+    """
+            await context.bot.send_message(chat_id=chat_from, text=new_full_text)
+            await update.message.delete()
+        return callback
 
 
-def get_forward_callback(chat_from, channel_to):
-    def callback(update, context):
-        author = update.message.from_user.name
-        text = update.message.text
-        full_text = f"""
-{author} shares:
-
-{text}
-"""
-        sent_message = context.bot.send_message(chat_id=channel_to, text=full_text)
-        new_full_text = f"""
-{full_text}
-
-Thread: {sent_message.link}
-"""
-        context.bot.send_message(chat_id=chat_from, text=new_full_text)
-        update.message.delete()
-    return callback
-
-
-for config in FORWARD_MAPPING:
-    callback = get_forward_callback(config["chat_from"], config["channel_to"])
-    filters = Filters.chat(config["chat_from"])
-    filter_tags = None
-    tags =  config.get("tags")
-    if tags is not None:
-        filter_tags = Filters.regex(tags[0])
-        for tag in tags[1:]:
-            filter_tags = filter_tags | Filters.regex(tag)
-    if filter_tags is not None:
-        filters = filters & filter_tags
-    handler = MessageHandler(filters, callback)
-    dispatcher.add_handler(handler)
-
-updater.start_polling()
+if __name__ == "__main__":
+    _bot = Forward22Bot()
+    _bot.run()
